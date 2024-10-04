97a224cc5fc5504dc6354edec6ba919e177c0cd1
Deydre <deydrealonso@gmail.com>
2024-10-04 20:05:56 +0200

    script.js refactorized into functions

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-// 1. Formulario de contacto - Local Storage
-// Crear un formulario de contacto con los siguientes campos:
-// Nombre
-// Email
-// Mensaje
-// URL imagen
+// If el array de localStorage existe y es mayor que 0, no borrarñp
+if (!localStorage.getItem("Contactos")) {
+    localStorage.setItem("Contactos", JSON.stringify([]));
+}
 
-// Guardar en Local Storage los datos de contacto enviados de cada usuario
-// Mostrar los datos de los contactos guardados en el DOM
-// Usa JSON.parse() y JSON.stringify() para guardar muchos datos usando la misma clave
-// Crea botón para borrar todos los contactos guardados en Local Storage
-// Crea botón para borrar un contacto en concreto de Local Storage.
-// NOTA IMPORTANTE: La estructura de datos de usuarios consistirá en un array de objetos [{..},{..},{..}...{..}] almacenado en Local Storage
-
-
+// Acceder al form y div del body y guardarlos
 let form = document.querySelector('form');
-
-let contacts = [];
-
-
-// PINTAR DATOS EN EL DOM
-// Acceder al div del body  y guardarlo
 let divLista = document.querySelector("#divLista");
-// Crear ul (le meteremos lis cada vez que hagamos submit)
+let contacts = [];
+// Crear ul y agregarla al div
 let ul = document.createElement("ul");
-// Agregar ul al div
 divLista.appendChild(ul);
+
 
 // Evento de submit
 form.addEventListener('submit', (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     // Accedemos a cada input del formulario
     let nombre = event.target.elements.name.value;
     let email = event.target.elements.email.value;
     let mensaje = event.target.elements.comments.value;
+    // IMAGEN PASARLO A "URL" EN EL FORMULARIO
     let imagen = event.target.elements.image.value;
 
     // Crear objeto
@@ -43,65 +30,81 @@ form.addEventListener('submit', (event) => {
         imagen: imagen
     }
 
-    // Meter el objeto dentro del array contacts
-    contacts.push(contact);
-    // Transformar el array a String y subirlo a Web Storage
-    localStorage.setItem("Contactos", JSON.stringify(contacts));
-
-    // PINTAR EL OBJETO EN EL DOM
-     // Creamos un li
-     let li = document.createElement("li");
-
-     // Con cada input
-     // Creamos un elemento p
-     let nombre1 = document.createElement("p");
-     // Le añadimos la info
-     nombre1.textContent = `Nombre: ${contact.nombre}`;
-
-     let email1 = document.createElement("p");
-     email1.textContent = `Email: ${contact.email}`;
-
-     let mensaje1 = document.createElement("p");
-     mensaje1.textContent = `Mensaje: ${contact.mensaje}`;
-
-     let imagen1 =document.createElement("p");
-     imagen1.textContent = `Imagen de ${contact.nombre}`
-
-     // Unimos todos los p al li
-     li.appendChild(nombre1);
-     li.appendChild(email1);
-     li.appendChild(mensaje1);
-     li.appendChild(imagen1);
-     // Unimos el li al ul
-     ul.appendChild(li);
+    guardarUser(contact);
+    pintarUser(contact);
 
 });
 
+// Función/ Subir user a Web Storage
+function guardarUser(contacto) {
+    contacts.push(contacto);
+    // Transformar el array a String y subirlo a Web Storage
+    localStorage.setItem("Contactos", JSON.stringify(contacts));
+}
 
-// Crea botón para borrar todos los contactos guardados en Local Storage Y EN EL dom
-// NUEVO ------------desde aquí --->
+// Función/ Get users de Web Storage
+function getUsers() {
+    let usersStorage = localStorage.getItem("Contactos");
+    // Transformar el array de vuelta porque estaba en String
+    return JSON.parse(usersStorage);
+}
+
+// Función/ Pintar un user en una tarjetita
+function pintarUser(contacto){
+
+    let li = document.createElement("li");
+
+    let nombre1 = document.createElement("p");
+    nombre1.textContent = `Nombre: ${contacto.nombre}`;
+
+    let email1 = document.createElement("p");
+    email1.textContent = `Email: ${contacto.email}`;
+
+    let mensaje1 = document.createElement("p");
+    mensaje1.textContent = `Mensaje: ${contacto.mensaje}`;
+
+    let imagen1 = document.createElement("p");
+    imagen1.textContent = `Imagen de ${contacto.nombre}`
+
+    // Unimos todos los p al li
+    li.appendChild(nombre1);
+    li.appendChild(email1);
+    li.appendChild(mensaje1);
+    li.appendChild(imagen1);
+    // Unimos el li al ul
+    ul.appendChild(li);
+}
+
+    
+
+// Crea botón para borrar todos los contactos guardados en Local Storage y en el DOM
 form.addEventListener('reset', (event) => {
-    event.preventDefault(); 
-    const numeroUsuario = event.target.elements.number.value;
-    let contactos = JSON.parse(localStorage.getItem("Contactos")) || [];
+    event.preventDefault();
+    const emailBorrar = event.target.elements.emailBorrar.value;
 
     // Si no se pone ningún numero
-    if (numeroUsuario === '') {
+    if (emailBorrar === '') {
         // Borrar todos los contactos
         localStorage.removeItem("Contactos");
-        // Borrar lo pitnado en el DOM
-        divLista.innerHTML = ''; 
-    // Si se pone un número
+        // Borrar lo pintado en el DOM
+        divLista.innerHTML = '';
+        // Si se pone un email
     } else {
-        // Filtrar para eliminar el de ese número 
-        console.log(numeroUsuario);
-        contacts = contactos.filter((contacto, i) => (i !== numeroUsuario + 1));
-        localStorage.setItem("Contactos", JSON.stringify(contacts));
+        // Bajar todo del local storage
+        // let listUsers = getUsers();
+        // // Filter y cargarse contacto
+        // let emailSeleccionado = listUsers.filter(user => user.email === emailBorrar);
+        // let indice = indexOf(listUsers.filter(user => user.email === emailBorrar));
+        // if (listUsers.includes(emailSeleccionado)){
+        //     // Borrar del array el índice del email seleccionado
+        //     listUsers.splice(indice, 1);
+        // }
+        // Volver a subir
 
+        // console.log(numeroUsuario);
+        // localStorage.setItem("Contactos", JSON.stringify(contacts));
 
     }
 });
 
-
-
-
+// USAR FUNCIONES
